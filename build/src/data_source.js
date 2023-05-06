eb52600b316de51fe9fd58fd4ea934ce56d7c9eb
WilberC1219 <wilberclaudio@gmail.com>
2023-05-05 22:03:25 -0400

    Latest build directory

--- build/src/data_source.js
+++ build/src/data_source.js
@@ -4,15 +4,15 @@ exports.AppDataSource = void 0;
 require("reflect-metadata");
 const typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "username",
-    password: "password",
-    database: "postgres",
-    synchronize: false,
-    logging: false,
-    entities: [__dirname + "/data/*.{js,ts}"],
-    subscribers: [],
-    migrations: [__dirname + "/migrations/*.{js,ts}"],
+  type: "postgres",
+  host: "localhost",
+  port: 3000,
+  username: "username",
+  password: "password",
+  database: "postgres",
+  synchronize: false,
+  logging: false,
+  entities: [__dirname + "/data/*.{js,ts}"],
+  subscribers: [],
+  migrations: [__dirname + "/migrations/*.{js,ts}"],
 });
